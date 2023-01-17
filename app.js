5481001557d8242746f788e0951a59803222f514
MargoKrk <malgorzata.walega@gmail.com>
2023-01-17 22:55:22 +0100

    add function on keys

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-function addPianoKey() {
+function addPianoKey(i) {
 
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("key");
+    const newDiv = document.createElement("div");//tu się tworzy div
+    newDiv.classList.add("key");//tu się wywołuje
+    newDiv.classList.add("key_number_" + i);//tu się wywołuje
+
+    newDiv.addEventListener("click", function () {
+        handleClick("white", i);
+    }); // nadanie funkcji po kliknięciu przycisku, robimy to w funkcji aby móc nadać argumenty
     const whiteKeysLayerElement = document.querySelector(".whiteKeysLayer"); //robimy sobie odnośnik do pianoTable
     whiteKeysLayerElement.appendChild(newDiv); //dodanie elementu do pianoTable
+
 
 }
 
 function allWhiteKeys(numberOfKeys) {
     let i = 0;
     while (i < numberOfKeys) {
-        addPianoKey();
+        addPianoKey(i);
         i++;
     }
 }
@@ -18,7 +24,7 @@ function allWhiteKeys(numberOfKeys) {
 function allBlackKeys(numberOfKeys) {
     let i = 0;
     while (i < numberOfKeys) {
-        addBlackKey();
+        addBlackKey(i);
         i++;
     }
 }
@@ -26,12 +32,20 @@ function allBlackKeys(numberOfKeys) {
 allWhiteKeys(21)
 allBlackKeys(21)
 
-function addBlackKey () {
+function addBlackKey(i) {
 
     const blackDiv = document.createElement("div");
     blackDiv.classList.add("blackKey");
+    blackDiv.addEventListener("click", function () {
+        handleClick("black", i);
+    });
+
     const blackKeysLayerElement = document.querySelector(".blackKeysLayer"); //robimy sobie odnośnik do pianoTable
     blackKeysLayerElement.appendChild(blackDiv);
 }
 
+
+function handleClick(color, i) {
+    console.log("zostałem dotknięty " + i, color)
+}
 
