23f96eeaf6b37f1638eddb621b89cbfde6b12d76
MargoKrk <malgorzata.walega@gmail.com>
2023-03-26 18:04:09 +0200

    P-014 addSound fnc

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 function addPianoKey(i) {
 
     const newDiv = document.createElement("div");//tu się tworzy div
-    newDiv.classList.add("key");//tu się wywołuje
-    newDiv.classList.add("key_number_" + i);//tu się wywołuje
+    newDiv.classList.add("whiteKey");//nadanie klasy
+    newDiv.classList.add("key_number_" + i);//nadanie klasy z numerem
 
     newDiv.addEventListener("click", function () {
         handleClick("white", i);
+        addSound()
     }); // nadanie funkcji po kliknięciu przycisku, robimy to w funkcji aby móc nadać argumenty
     const whiteKeysLayerElement = document.querySelector(".whiteKeysLayer"); //robimy sobie odnośnik do pianoTable
     whiteKeysLayerElement.appendChild(newDiv); //dodanie elementu do pianoTable
-
-
 }
 
 function allWhiteKeys(numberOfKeys) {
@@ -38,6 +37,7 @@ function addBlackKey(i) {
     blackDiv.classList.add("blackKey");
     blackDiv.addEventListener("click", function () {
         handleClick("black", i);
+        addSound()
     });
 
     const blackKeysLayerElement = document.querySelector(".blackKeysLayer"); //robimy sobie odnośnik do pianoTable
@@ -46,6 +46,16 @@ function addBlackKey(i) {
 
 
 function handleClick(color, i) {
-    console.log("zostałem dotknięty " + i, color)
+    console.log("zostałem dotknięty " + (i + 1), color)
 }
+
+function addSound () {
+
+    // let audio = new Audio("./sound/white/1.C3.mp3");
+
+    let audio = new Audio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
+
+        return audio.play()
+}
+
 
