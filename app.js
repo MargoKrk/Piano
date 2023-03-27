2e461aef84f39288aba19c0420cf035472f92a90
MargoKrk <malgorzata.walega@gmail.com>
2023-03-27 23:41:17 +0200

    P-014 Add class and name to keys

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function allBlackKeys(numberOfKeys) {
     }
 }
 
-allWhiteKeys(21)
+allWhiteKeys(21);
 allBlackKeys(21)
 
 function addBlackKey(i) {
@@ -46,16 +46,43 @@ function addBlackKey(i) {
 
 
 function handleClick(color, i) {
+
     console.log("zostałem dotknięty " + (i + 1), color)
 }
 
 function addSound () {
 
-    // let audio = new Audio("./sound/white/1.C3.mp3");
-
-    let audio = new Audio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
+    let audio = new Audio("./sound/white/1. C3.mp3");
 
         return audio.play()
 }
+
+function addNoteNameToWhiteKeys() {
+
+    const noteNames = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5"];
+    const allWhiteKeys = document.querySelectorAll(".whiteKey");
+
+    allWhiteKeys.forEach((whiteKey, i) => {
+
+        whiteKey.innerHTML = noteNames[i];
+        whiteKey.classList.add(noteNames[i])
+    })
+}
+
+function addNoteNameToBlackKeys() {
+
+    const noteNames = ["C#3", "D#3", "", "F#3", "G#3", "A#3", " ", "C#4", "D#4", " ", "F#4", "G#4", "A#4", " ", "C#5", "D#5", " ", "F#5", "G#5", "A#5", " ",];
+    const allBlackKeys = document.querySelectorAll(".blackKey");
+
+    allBlackKeys.forEach((blackKey, i) => {
+
+        blackKey.innerHTML = noteNames[i];
+        blackKey.classList.add(noteNames[i])
+    })
+}
+
+
+addNoteNameToWhiteKeys();
+addNoteNameToBlackKeys();
 
 
